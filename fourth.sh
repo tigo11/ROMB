6f780ee33d5b1a789d3e4727cf92407d0edbffd9
tigo11 <tigrulia_442@mail.ru>
2022-10-18 19:08:41 +0000

    rmb

---
+++ fourth.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+size=$1
+
+#first part of the romb
+part1=1
+for ((i=0;i<size;i++))
+do
+for ((j=0;j<((size-part1));j++))
+do
+echo -n " "
+done
+
+for ((k=0;k<$part1;k++))
+do
+echo -n "* "
+done
+
+echo ""
+part1=$(($part1+1))
+done
+
+#second part of the romb
+part2=size-1
+for ((i=0;i<=((size-1));i++))
+do
+for ((j=0;j<((size-part2));j++))
+do
+echo -n " "
+done
+
+for ((k=0;k<part2;k++))
+do
+echo -n "* "
+done
+
+echo ""
+part2=$(($part2-1))
+done
+
+exit 0
