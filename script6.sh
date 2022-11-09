588f2e4f2e2120a19485c4185bf0185955c4b952
tigo11 <91616660+tigo11@users.noreply.github.com>
2022-11-09 16:19:03 +0300

    Update script6.sh

--- script6.sh
+++ script6.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #the last incoming always!!!!!!!!! sits down penultimate
+
+#processing parameters
 if [ $# -eq 0 ]; then
 	echo "How much chairs?"
 	exit 1
 fi
+
+#main programm
 count=$1
 let a=($count - 1 )
 echo $a
